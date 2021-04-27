d82bbccd7887a75f760eb9037906b11fd2ce6b67
Nikita Trotner <nikita@ttnr.me>
2021-04-27 22:10:29 +0200

    general user interface

---
+++ server/database/interfaces/user_interface.ts
@@ -0,0 +1,15 @@
+import { Document } from 'mongoose';
+
+export interface IUser extends Document {
+  username: string;
+  email: string;
+  password_hash: string;
+  about: string;
+}
+
+export const User_Definitions = {
+  username: {type: String, required: true},
+  email: {type: String, required: true},
+  password_hash: {type: String, required: true},
+  about: {type: String, required: false, default: ''},
+}
